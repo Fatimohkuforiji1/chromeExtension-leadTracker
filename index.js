8277a0229936358a88d840b93c4f3841a9305963
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-09-19 23:49:48 +0100

    Iterated through the push value using loop

---
+++ index.js
@@ -0,0 +1,33 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el"); //const means name can't be reassign
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+//add event listener to the input button to replace onclick on html
+inputBtn.addEventListener("click", function () {
+
+  myLeads.push(inputEl.value);
+  inputEl.value = ""; //clear input field
+
+  renderLeads();
+
+});
+
+function renderLeads(){
+let listItems = "";
+  for(let i =0; i < myLeads.length; i++){
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li) same as
+
+  //listItems += "<li><a target='_blank' href = '" + myLeads[i] + "' >" +  myLeads[i] +"</a></li>";
+    listItems +=`
+    <li>
+        <a target='_blank' href = '${myLeads[i]}'>
+          ${myLeads[i]}
+          </a>
+    </li>`
+
+  } 
+ulEl.innerHTML = listItems;  
+}
