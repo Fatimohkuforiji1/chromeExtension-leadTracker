9eaac3b0e79c7c44839cf5dc1fca84b9e6721e13
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-09-21 11:10:16 +0100

    Deployed to chrome extension

--- index.js
+++ index.js
@@ -4,30 +4,39 @@ const inputEl = document.getElementById("input-el"); //const means name can't be
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+//local storage saves our work on chrome
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+ console.log(leadsFromLocalStorage);
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 //add event listener to the input button to replace onclick on html
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = ""; //clear input field
-  renderLeads();
 
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+ 
 });
 
-function renderLeads(){
-let listItems = "";
-  for(let i =0; i < myLeads.length; i++){
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
     // const li = document.createElement("li")
     // li.textContent = myLeads[i]
     // ulEl.append(li) same as
 
-  //listItems += "<li><a target='_blank' href = '" + myLeads[i] + "' >" +  myLeads[i] +"</a></li>";
-    listItems +=`
+    //listItems += "<li><a target='_blank' href = '" + myLeads[i] + "' >" +  myLeads[i] +"</a></li>";
+    listItems += `
     <li>
         <a target='_blank' href = '${myLeads[i]}'>
           ${myLeads[i]}
           </a>
     </li>
-    `
-
-  } 
-ulEl.innerHTML = listItems;  
+    `;
+  }
+  ulEl.innerHTML = listItems;
 }
